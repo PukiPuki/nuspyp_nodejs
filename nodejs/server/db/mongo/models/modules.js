47b3b4e5d26df7bea9490df1984cf0569c4af82e
PukiPuki <dfwong5566@gmail.com>
2017-06-26 06:06:53 +0000

    remove

--- nodejs/server/db/mongo/models/modules.js
+++ nodejs/server/db/mongo/models/modules.js
@@ -5,12 +5,27 @@
 import mongoose from 'mongoose';
 
 const ModuleSchema = new mongoose.Schema({
-  id: String,
-  text: String,
-  count: { type: Number, min: 0 },
-  date: { type: Date, default: Date.now }
+  code: String,
+  papers: [],
+  last_update: { type: Date, default: Date.now }
 });
 
+const paper_schema = new mongoose.Schema({
+  //20171 = 2017 sem 1
+  //20172 = 2017 sem 2
+  year: Number,
+  //
+  file: mongoose.Schema.Types.ObjectId
+})
+
+const thread_schema = new mongoose.Schema({
+  title: String,
+  description: String,
+  tag: [String],
+  author: String,
+  thumbnail: mongoose.Schema.Types.ObjectId,
+
+})
 // Compiles the schema into a model, opening (or creating, if
 // nonexistent) the 'Topic' collection in the MongoDB database
 export default mongoose.model('Topic', TopicSchema);
