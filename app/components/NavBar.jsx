615fdfea760a513ed656342bff2ed2b2a3d44686
derrick <derrick.2a4@gmail.com>
2017-07-12 22:25:30 +0800

    Added NavBar, working on Search bar

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router';
+import styles from '../css/components/navigation';
+import classNames from 'classnames/bind';
+
+// Material Ui
+import ReactDOM from 'react-dom';
+import FlatButton from 'material-ui/FlatButton'
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import SearchBar from 'components/SearchBar';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+
+const cx = classNames.bind(styles);
+
+
+export default class DrawerUndockedExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+	const {user, logOut} = this.props;
+    return (
+      <div>
+        	<Drawer
+        	  docked={false}
+          	width={200}
+          	open={this.state.open}
+          	onRequestChange={(open) => this.setState({open})}
+        	>
+          	<MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+        	</Drawer>
+
+				<AppBar
+					title="NUSPYP" 
+					onLeftIconButtonTouchTap={this.handleToggle}> 
+				<ToolbarGroup>
+			
+				<SearchBar />
+				</ToolbarGroup>
+				<ToolbarGroup>
+				{ user.authenticated 
+							? (<FlatButton label="Logout" 
+									href="/" onTouchTap={logOut} style={{color: "white"}} />) 
+							: (<FlatButton label="Login" href="/login" style={{color: "white"}}/>)
+						} 
+
+			</ToolbarGroup>
+			</AppBar>
+      </div>
+		);
+  }
+}
+
+
